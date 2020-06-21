8d4828d50862e429b9db6d7b77300b174b78fb7a
rixterx1 <67139030+rixterx1@users.noreply.github.com>
2020-06-21 11:50:03 +0545

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
@@ -11,7 +12,10 @@ chrome_options.add_argument("--allow-popups-during-page-unload")
 chrome_options.add_argument("--enable-auto-reload")
 driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
+
 driver.maximize_window()
+
+
 driver.execute_script("window.open('');")
 driver.switch_to.window(driver.window_handles[1])
 driver.get("https://t.co/HlVUCorAUc?amp=1")
